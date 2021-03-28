ccaa4b21cc7bbd2874f8526a35246b2d63f219c3
platoscave <platoscave60@gmail.com>
2021-03-28 07:32:24 +0200

    working on classmodel

--- src/lib/classObject3d.js
+++ src/lib/classObject3d.js
@@ -17,8 +17,10 @@ export default class ClassObject3d extends THREE.Object3D {
     this.name = userData.label
     this.userData = userData
 
+    const { class: assocProps } = classModelColors 
+    const material = new THREE.MeshLambertMaterial({ color: assocProps.color })
 
-    let mesh = new THREE.Mesh(this.getGeometry(), this.getMaterial())
+    let mesh = new THREE.Mesh(this.getGeometry(this.userData.docType), material)
     mesh.name = userData.label + ' - 3d mesh'
     this.add(mesh)
     selectableMeshArr.push(mesh)
@@ -30,12 +32,12 @@ export default class ClassObject3d extends THREE.Object3D {
     // Draw up beam from here to middle
     if (yPos !== 0) {
       let p2 = new THREE.Vector3(0, HEIGHT * 2, 0)
-      const parentBeamMesh = this.drawBeam(new THREE.Vector3(0, 0, 0), p2, this.mapAssocNameToMaterial())
+      const parentBeamMesh = this.drawBeam(new THREE.Vector3(0, 0, 0), p2, 'classConnectors')
       this.add(parentBeamMesh)
     }
 
     this.position.setY(yPos);
-    this.updateMatrix()
+    //this.updateMatrix()
 
     console.log('    ', this._id, this.name, yPos)
 
@@ -59,20 +61,25 @@ export default class ClassObject3d extends THREE.Object3D {
 
     // Collect userData for use durring creation
     const userDataArr = result.docs.map((item) => {
-      //console.log("item - " +idx, item);
+      let assocs = []
+      for (let key in item.properties) {
+        const prop = item.properties[key]
+        if (prop.mongoQuery) assocs.push({ name: key, destId: prop.mongoQuery.selector.classId })
+      }
       return {
         _id: item._id,
         label: item.title ? item.title : item.name,
         subQueryIds: queryObjClone.subQueryIds,
         pageId: item.pageId ? item.pageId : queryObjClone.pageId,
         docType: item.docType,
+        assocs: assocs
       };
     });
 
     // If this class has children, draw down beam from here to middle
     if (userDataArr.length) {
       let p2 = new THREE.Vector3(0, -HEIGHT * 2, 0)
-      const childrenBeamMesh = this.drawBeam(new THREE.Vector3(0, 0, 0), p2, this.mapAssocNameToMaterial())
+      const childrenBeamMesh = this.drawBeam(new THREE.Vector3(0, 0, 0), p2, 'classConnectors')
       this.add(childrenBeamMesh)
     }
 
@@ -84,8 +91,8 @@ export default class ClassObject3d extends THREE.Object3D {
       this.add(classObj3d)
 
       // Tell the child to draw its children
-      if(classObj3d._id !== '5jdnjqxsqmgn') // skip everything under Balance Sheet
-      childrenPronmises.push(classObj3d.drawChildren(selectableMeshArr, queryObj, pouch))
+      if (classObj3d._id !== '5jdnjqxsqmgn') // skip everything under Balance Sheet
+        childrenPronmises.push(classObj3d.drawChildren(selectableMeshArr, queryObj, pouch))
     })
 
 
@@ -122,92 +129,68 @@ export default class ClassObject3d extends THREE.Object3D {
     let firstX = -1, lastX = -1
     this.children.forEach((subClassObj3d) => {
       if (subClassObj3d.type === 'Object3D') {
-        if(firstX === -1) firstX = subClassObj3d.position.x
+        if (firstX === -1) firstX = subClassObj3d.position.x
         lastX = subClassObj3d.position.x
       }
     });
 
     // Draw the horizontal beam, based on the positions of the first and last child
-    let q1 = new THREE.Vector3(firstX, -HEIGHT * 2, 0)
-    let q2 = new THREE.Vector3(lastX, -HEIGHT * 2, 0)
-    const horizontalBeamMesh = this.drawBeam(q1, q2, this.mapAssocNameToMaterial(), true)
-    this.add(horizontalBeamMesh)
 
-    // return the original x plus our children max, so that the parent can 
+    if (firstX !== -1) {
+      let q1 = new THREE.Vector3(firstX, -HEIGHT * 2, 0)
+      let q2 = new THREE.Vector3(lastX, -HEIGHT * 2, 0)
+      this.add(this.drawBeam(q1, q2, 'classConnectors', true))
+    }
+
+    // return the original x plus our children max, so that the parent can center itself
     return xPos + childrenMaxX
   }
 
-  drawClassBeams() {
-    if (this.subclassesObj3ds.length > 0) {
-      let connectorMaterial = this.mapAssocNameToMaterial()
+  drawClassAssocs(glModelObject3D) {
 
-      // matrix to translate positions to our local coordinates
-      let ourInverseMatrix = new THREE.Matrix4().getInverse(this.matrix)
+    for(let key in this.userData.assocs) {
 
-      // vertical beam from parent class to horizontal beam
-      let parentEndPosition = new THREE.Vector3(0, HEIGHT * -2, 0)
-      this.add(this.drawBeam(new THREE.Vector3(), parentEndPosition, connectorMaterial))
+      const assoc = this.userData.assocs[key]
 
-      // horizontal beam
-      let beamStartPos = this.subclassesObj3ds[0].position.clone()
-      beamStartPos.applyMatrix4(ourInverseMatrix)
-      beamStartPos.setY(HEIGHT * -2)
-      let beamEndPos = this.subclassesObj3ds[this.subclassesObj3ds.length - 1].position.clone()
-      beamEndPos.applyMatrix4(ourInverseMatrix)
-      beamEndPos.setY(HEIGHT * -2)
-      this.add(this.drawBeam(beamStartPos, beamEndPos, connectorMaterial))
+      const { [assoc.name]: assocProps } = classModelColors 
+      if(!assocProps) continue
+      const depth = assocProps.depth * -DEPTH - DEPTH
 
-      // sphere at the left end
-      let sphereGeometryLeft = new THREE.SphereGeometry(15)
-      let sphereMeshLeft = new THREE.Mesh(sphereGeometryLeft, connectorMaterial)
-      sphereMeshLeft.position.set(beamStartPos.x, beamStartPos.y, beamStartPos.z)
-      this.add(sphereMeshLeft)
-      // sphere at the right end
-      let sphereGeometry = new THREE.SphereGeometry(15)
-      let sphereMesh = new THREE.Mesh(sphereGeometry, connectorMaterial)
-      sphereMesh.position.set(beamEndPos.x, beamEndPos.y, beamEndPos.z)
-      this.add(sphereMesh)
+      const p1 = new THREE.Vector3(0, 0, -DEPTH/2)
+      const p2 = new THREE.Vector3(0, 0, depth)
+      this.add(this.drawBeam(p1, p2, assoc.name))
 
-      // for each of the subclasses
-      this.subclassesObj3ds.forEach(childObj3d => {
-        // beam from child class to horizontal beam
-        let childBeamStartPos = childObj3d.position.clone()
-        childBeamStartPos.applyMatrix4(ourInverseMatrix)
-        childBeamStartPos.setY(HEIGHT * -2)
-        let childBeamEndPos = childObj3d.position.clone()
-        childBeamEndPos.applyMatrix4(ourInverseMatrix)
-        childBeamEndPos.setY(HEIGHT * -4)
-        this.add(this.drawBeam(childBeamStartPos, childBeamEndPos, connectorMaterial))
+      const destObj3d = glModelObject3D.getObjectByProperty('_id', assoc.destId)
+      if(!destObj3d) console.log(assoc)
+      if(!destObj3d) continue
 
-        // tell the subclass to draw it's class beams
-        childObj3d.drawClassBeams()
-      })
+      //let destP1 = destObj3d.position.clone()
+
+      let destP1 = destObj3d.position.clone()
+      //destP1.applyMatrix4(destObj3d.matrixWorld)
+
+
+      destP1.z = -DEPTH/2
+
+      //let destP1 = destObj3d.position.clone()
+
+      // Apply the mesh's world matrix to translate to world coords
+      //destP1.applyMatrix4(destObj3d.matrixWorld)
+      let destP2 = destP1.clone()
+      destP2.z = depth
+
+      this.add(this.drawBeam(destP1, destP2, assoc.name))
+      this.add(this.drawBeam(p2, destP2, assoc.name))
+
+      console.log(destObj3d)
+      
     }
-  }
-  drawClassAssocs(placeholderObj3d) {
-    const getAssocs = (properties) => {
-      let resultsArr = []
-      Object.keys(properties).forEach(key => {
-        let obj = properties[key]
-        let toKey = ''//_.get(obj, 'query.from')
-        if (toKey && toKey !== 'classes') resultsArr.push({ _id: _id, name: obj.title, XXXkey: toKey })
-        let subProperties = ''//_.get(obj, 'items.properties')
-        if (subProperties) resultsArr = resultsArr.concat(getAssocs(subProperties))
-      })
-      return resultsArr
-    }
+    this.children.forEach((subClassObj3d) => {
+      if (subClassObj3d.type === 'Object3D') {
+        subClassObj3d.drawClassAssocs(glModelObject3D) 
+      }
+    });
 
-    let properties = this.userData.properties
-    if (!properties) return
-    let assocsArr = getAssocs(properties)
-    console.log('assocsArr', assocsArr)
-
-    assocsArr.forEach(assoc => {
-      let assocToObj3d = placeholderObj3d.getObjectByProperty('_id', assoc._id)
-
-      if (!assocToObj3d) console.error('Assoc destination mot found', assoc)
-      else this.drawTubeTopSideToBottom(assocToObj3d, assoc._id)
-    })
   }
   drawObjectToClassBeam(length) {
     let fomPos = new THREE.Vector3(0, -HEIGHT / 4, 0)
@@ -242,7 +225,11 @@ export default class ClassObject3d extends THREE.Object3D {
       else this.drawTubeTopSideToBottom(assocToObj3d, assoc.name)
     })
   }
-  drawBeam(p1, p2, material, caps) {
+  drawBeam(p1, p2, name, caps) {
+
+    const { [name]: assocProps = { color: 0xEFEFEF }  } = classModelColors 
+    const material = new THREE.MeshLambertMaterial({ color: assocProps.color })
+    
     let geometries = []
 
     // https://stackoverflow.com/questions/15139649/three-js-two-points-one-cylinder-align-issue/15160850#15160850
@@ -276,6 +263,26 @@ export default class ClassObject3d extends THREE.Object3D {
     return new THREE.Mesh(mergedGeometry, material)
 
   }
+  drawTube(points, name, arrow){
+
+    const { [name]: assocProps = { color: 0xEFEFEF }  } = classModelColors 
+    const material = new THREE.MeshLambertMaterial({ color: assocProps.color })
+    
+    let path = new THREE.CatmullRomCurve3(this.straightenPoints(points))
+    // path.curveType = 'centripetal';
+    let geometry = new THREE.TubeGeometry(path, 300, 7, 8, false)
+    let mesh = new THREE.Mesh(geometry, material)
+    this.add(mesh)
+
+    if(arrow) {
+      let coneGeometry = new THREE.CylinderGeometry(0, 40, 100, 40, 40, false)
+      let coneMesh = new THREE.Mesh(coneGeometry, material)
+      this.add(coneMesh)
+    }
+
+    if(name) this.addTextMeshBetween(name, points[1], points[2])
+  }
+
   drawTubeTopSideToBottom(toObj3d, name) {
     // translate toPosition to our local coordinates
     let toPosition = toObj3d.position.clone()
@@ -357,22 +364,6 @@ export default class ClassObject3d extends THREE.Object3D {
 
     this.addTextMeshBetween(name, points[1], points[2])
   }
-  mapAssocNameToMaterial(name) {
-    let nameColor = classModelColors.nameColor[name]
-    if (nameColor) return new THREE.MeshLambertMaterial({ color: nameColor.color })
-    if (name) console.warn('Add color to classModelColors', name)
-    return new THREE.MeshLambertMaterial({ color: 0xEFEFEF }) // grey, class connectors
-  }
-  mapAssocNameToDepth(name) {
-    let nameColor = classModelColors.nameColor[name]
-    if (nameColor) return nameColor.depth
-    return 0
-  }
-  getMaterial() {
-    let nameColor = classModelColors.nameColor[this.userData.docType]
-    if (nameColor) return new THREE.MeshLambertMaterial({ color: nameColor.color })
-    return new THREE.MeshLambertMaterial({ color: 0x00A300 }) // green
-  }
   getSidePos(side, pos) {
     if (side === 'top') return new THREE.Vector3(pos.x, pos.y + HEIGHT / 2, pos.z)
     if (side === 'right') return new THREE.Vector3(pos.x + WIDTH / 2, pos.y, pos.z)
@@ -382,11 +373,11 @@ export default class ClassObject3d extends THREE.Object3D {
     if (side === 'back') return new THREE.Vector3(pos.x, pos.y, pos.z - DEPTH / 2)
     return pos
   }
-  getGeometry() {
+  getGeometry(docType) {
     const x = 0, y = 0
 
     let shape = new THREE.Shape()
-    if (this.userData.docType === 'class')
+    if (docType === 'class')
       shape.moveTo(x, y + HEIGHT / 3)
         .lineTo(x, (y + HEIGHT / 3) * 2)
         .lineTo(x + WIDTH / 2, y + HEIGHT)
