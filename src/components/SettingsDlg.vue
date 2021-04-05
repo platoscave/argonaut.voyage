7a80a8e7cb0d5ad23e3a7b032e660a48b902fd25
platoscave <platoscave60@gmail.com>
2021-04-05 15:19:54 +0200

    fixed import

--- src/components/SettingsDlg.vue
+++ src/components/SettingsDlg.vue
@@ -13,11 +13,15 @@
         >Poputate Cache From Static</el-button
       >
     </el-row>
+    </el-row>
+      <el-button type="primary" @click="test"
+        >Test</el-button
+      >
+    </el-row>
   </el-dialog>
 </template>
 
 <script>
-import argo from "../assets/argonautstatic.js";
 
 export default {
   name: "settings-dlg",
@@ -26,91 +30,45 @@ export default {
   },
 
   methods: {
-    clearCache() {
-      this.$pouch
-        .destroy()
-        .then( () => {
-          this.$message({ message: "Cache Cleared", type: 'success' });
-        })
-        .catch( err => {
-          this.$message({ message: err, type: "error"});
-        });
+    async clearCache() {
+      try {
+        await this.$pouch.destroy()
+        this.$message({ message: "Cache Cleared", type: "success" })
+      } catch (err) {
+        this.$message({ message: err, type: "error" })
+      }
     },
-
-    populateFromStatic() {
-      this.$pouch
-        .bulkDocs(argo)
-        .then(() => {
-          this.$message({ message: "Static File Loaded", type: 'success' });
-        })
-        .catch(function (err) {
-          this.$message({ message: err, type: "error" });
-        });
-
-      this.$pouch
-        .createIndex({
+    async populateFromStatic() {
+      try {
+        const response = await fetch("argonaut.json")
+        const argonautData = await response.json()
+        await this.$pouch.destroy()
+        await this.$pouch.bulkDocs(argonautData)
+        await this.$pouch.createIndex({
           index: {
             fields: ["parentId"],
           },
         })
-        .then(() => {
-          this.$message({ message: "Index Created: parentId", type: 'success' });
-        })
-        .catch((err) => {
-          this.$message({ message: err, type: "error" });
-        });
-
-      this.$pouch
-        .createIndex({
+        await this.$pouch.createIndex({
           index: {
             fields: ["classId"],
           },
         })
-        .then(() => {
-          this.$message({ message: "Index Created: classId", type: 'success' });
-        })
-        .catch((err) => {
-          this.$message({ message: err, type: "error" });
-        });
-
-      this.$pouch
-        .createIndex({
+        await this.$pouch.createIndex({
           index: {
             fields: ["ownerId"],
           },
         })
-        .then(() => {
-          this.$message({ message: "Index Created: ownerId", type: 'success' });
-        })
-        .catch((err) => {
-          this.$message({ message: err, type: "error" });
-        });
+        await this.$message({ message: "Static File Loaded and Imported", type: "success" })
 
-      this.$pouch
-        .createIndex({
-          index: {
-            fields: ["name"],
-          },
-        })
-        .then(() => {
-          this.$message({ message: "Index Created: name", type: 'success' });
-        })
-        .catch((err) => {
-          this.$message({ message: err, type: "error" });
-        });
+      } catch (err) {
+        console.error(err);
+        this.$message({ message: err, type: "error" });
+      }
 
-      this.$pouch
-        .createIndex({
-          index: {
-            fields: ["title"],
-          },
-        })
-        .then(() => {
-          this.$message({ message: "Index Created: title", type: 'success', duration: 0});
-        })
-        .catch((err) => {
-          this.$message({ message: err, type: "error" });
-        });
+    },
+    test() {
+ 
     },
   },
 };
